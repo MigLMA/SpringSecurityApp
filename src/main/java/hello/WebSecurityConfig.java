8aa3223c226113a89f07ad14d3a2a231d13f1a61
Miguel Marcos <Miguel.Marcos@cognizant.com>
2017-10-11 17:49:21 +0200

    ﻿[Add] Modify the security configuration class to use the jdbc datasource we have defined for authenticating and authorize users.
    [Add] Remove inMemory Authentication by removing method configureGlobal.
    [Add] Add Autowired-annotated javax.sql.datasource variable.
    [Add] Modify configure(http) method to setup authentication on pages.

--- src/main/java/hello/WebSecurityConfig.java
+++ src/main/java/hello/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package hello;
 
+import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -11,19 +12,53 @@ import org.springframework.security.config.annotation.web.servlet.configuration.
 @Configuration
 @EnableWebMvcSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().formLogin()
-				.loginPage("/login").permitAll().and().logout().permitAll();
-	}
 
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
-	}
+  private static final String selectUserQuery =
+      "SELECT username,password, enabled FROM users WHERE username=?";
+  private static final String authorizationUserQuery =
+      "SELECT username, role FROM user_roles WHERE username=?";
 
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/css/*");
-	}
+
+  @Autowired
+  DataSource dataSource;
+
+
+  @Autowired
+  public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+    auth.jdbcAuthentication().dataSource(dataSource)
+        .usersByUsernameQuery(WebSecurityConfig.selectUserQuery)
+        .authoritiesByUsernameQuery(WebSecurityConfig.authorizationUserQuery);
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    // XXX _NOTE:
+    // Configuration that is specific to certain pages or urls MUST BE placed FIRST
+    // than configurations that are common among most urls.
+
+ // @formatter:// @formatter:off
+    http.authorizeRequests()
+      .antMatchers("/", "/home").access("hasRole('ROLE_USER')")
+      .antMatchers("/hello").access("hasRole('ROLE_ADMIN')")
+// .anyRequest().permitAll()
+      .anyRequest().authenticated()
+      .and()
+      .formLogin()
+        .loginPage("/login").permitAll()
+      .and()
+        .logout().logoutSuccessUrl("/login?logout")
+      .and()
+        .exceptionHandling().accessDeniedPage("/403")
+      .and()
+      .csrf();
+// @formatter:on
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().antMatchers("/css/*");
+  }
+
+
+
 }
