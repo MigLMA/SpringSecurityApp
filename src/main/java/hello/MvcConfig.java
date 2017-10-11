6694e848188f28b6105863d8ae008f4e12dc5398
Miguel Marcos <Miguel.Marcos@cognizant.com>
2017-10-11 17:29:19 +0200

    ﻿[Add] Datasource for DB.
    [Add] Provide a definition to the mysql datasource in our MvcConfig class which has all necessary information to connect to the database we created before.
    [Add] Register a view for "403" (access denied) page. This page will be displayed whenever an user tries to access a page he/she is not authorized to.

--- src/main/java/hello/MvcConfig.java
+++ src/main/java/hello/MvcConfig.java
@@ -2,7 +2,7 @@ package hello;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -10,21 +10,30 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
-        registry.addViewController("/login").setViewName("login");
-    }
-    
-    @Bean
-	public InternalResourceViewResolver viewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/jsp/");
-		resolver.setSuffix(".jsp");
-		return resolver;
-	}
-    
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/home").setViewName("home");
+    registry.addViewController("/").setViewName("home");
+    registry.addViewController("/hello").setViewName("hello");
+    registry.addViewController("/login").setViewName("login");
+    registry.addViewController("/403").setViewName("403");
+  }
 
+  @Bean
+  public InternalResourceViewResolver viewResolver() {
+    InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+    resolver.setPrefix("/WEB-INF/jsp/");
+    resolver.setSuffix(".jsp");
+    return resolver;
+  }
+
+  @Bean(name = "dataSource")
+  public DriverManagerDataSource dataSource() {
+    DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+    driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+    driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/helloDB?useUnicode=true");
+    driverManagerDataSource.setUsername("root");
+    driverManagerDataSource.setPassword("root1234");
+    return driverManagerDataSource;
+  }
 }
